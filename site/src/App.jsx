2426e3d57b64cff8665b450701167e331858e322
Preston Button <prestonbutton99@gmail.com>
2023-03-06 11:43:10 -0500

    get rough header and pre-router navbar

--- site/src/App.jsx
+++ site/src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
+import Header from './components/Header/Header';
 
 function App() {
 
   return (
     <div className="App">
       <Navbar />
-      {/* <h1>preston button</h1> */}
+      <Header />
     </div>
   )
 }
