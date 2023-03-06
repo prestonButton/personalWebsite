136e667afa2846405ecc9d3798625ad319546dff
Preston Button <prestonbutton99@gmail.com>
2023-03-06 15:40:11 -0500

    Add Icons to header

--- site/src/overlay.jsx
+++ site/src/overlay.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Icons from './Icons';
 import './Overlay.css';
 
 export default function Overlay() {
@@ -8,6 +9,7 @@ export default function Overlay() {
         <p className="Header--name">Preston Button</p>
         <p className="Header--title">Frontend Developer</p>
       </div>
+      <Icons />
     </div>
   );
 }
